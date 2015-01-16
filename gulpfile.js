1d2ec6fd40f32b5f88e7f0b723fc3f87b1ed1607
rzou <rzou@Ctrip.com>
2015-01-16 14:37:17 +0800

    sass server default

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,22 @@
 var gulp = require('gulp'),
-    watch = require('gulp-watch'),
     server = require('gulp-develop-server'),
     plugins = require('gulp-load-plugins')();
 
-gulp.task('default', ['server'], function() {
-    //watch sass change & compile
+// 启动
+gulp.task('default', ['server','styles'], function() {
+    gulp.watch('./web/public/scss/*.scss', ['styles']);
+    gulp.watch('./web/public/img/**', ['images']);
+});
+
+// watch sass change & compile
+gulp.task('styles', function() {
     gulp.src('./web/public/scss/*.scss')
-        .pipe(watch('./web/public/scss/*.scss'))
+        .pipe(plugins.plumber())
         .pipe(plugins.sass())
         .pipe(gulp.dest('./web/public/.css/'));
 });
 
-//start server
+// start express server
 gulp.task('server', function() {
     server.listen({path: './web/app.js'});
 });
@@ -20,3 +25,4 @@ gulp.task('server', function() {
 gulp.task('server:restart', function() {
     gulp.watch(['./web/app.js'], server.restart);
 });
+
