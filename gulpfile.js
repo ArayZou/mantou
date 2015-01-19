77e63b859071c03ed27ba4e2a6901d0d6de02ff6
rzou <rzou@Ctrip.com>
2015-01-19 11:37:33 +0800

    mantou.css import

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,26 @@
 var gulp = require('gulp'),
     server = require('gulp-develop-server'),
     minifyCSS = require('gulp-minify-css'),
-    concat = require('gulp-concat'),        // 合并文件
+    // concat = require('gulp-concat'),        // 合并文件
     rename = require('gulp-rename'),        // 重命名
     plugins = require('gulp-load-plugins')();
 
 // 启动
 gulp.task('default', ['server','styles'], function() {
-    gulp.watch('./web/public/scss/*.scss', ['build-sass']);
-    gulp.watch('./web/public/css/*.css', ['styles']);
+    gulp.watch('./web/public/scss/*.scss', ['styles']);
 });
 
 // watch sass change & compile
-gulp.task('build-sass', function() {
-    gulp.src('./web/public/scss/*.scss')
+gulp.task('styles', function() {
+    gulp.src('./web/public/scss/mantou.scss')
         .pipe(plugins.plumber())
         .pipe(plugins.sass())
         .pipe(gulp.dest('./web/public/css/'));
 });
 
-gulp.task('styles',['build-sass'], function() {
-  gulp.src('./web/public/css/*.css')
-    .pipe(concat('all.css'))
+//minify css
+gulp.task('minify-css',['styles'], function() {
+  gulp.src('./web/public/css/mantou.css')
     .pipe(gulp.dest('./web/public/css/'))
     .pipe(rename({ suffix: '.min' }))
     .pipe(minifyCSS({keepBreaks:true}))
