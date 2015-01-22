735f7c9c3799ac904ea5e7022a1da7f378feaced
rzou <rzou@Ctrip.com>
2015-01-22 18:07:56 +0800

    home page && getimg

--- web/controllers/site.js
+++ web/controllers/site.js
@@ -1,3 +1,4 @@
+//index页
 exports.index = function (req, res, next) {
     // 存DB
     var models = require('../models');
@@ -10,4 +11,14 @@ exports.index = function (req, res, next) {
     });
     // 渲染
     res.render('index', {title: 'index'});
+}
+
+//home页
+exports.home = function (req, res, next) {
+    var ifLogin = Math.random()>0.5?true:false;
+    // 渲染
+    res.render('home', {
+        title: 'home',
+        ifLogin: ifLogin
+    });
 }
--- web/router.js
+++ web/router.js
@@ -2,13 +2,18 @@ var express = require('express'),
     site = require('./controllers/site'),
     user = require('./controllers/user'),
     json = require('./controllers/json'),
+    getimg = require('./controllers/getimg'),
     router = express.Router();
 
 // index
 router.get('/', site.index);
+// home
+router.get('/home', site.home);
 // user
 router.get('/user', user.index);
 // json
 router.get('/json/:jsonfunc', json.index);
+// getimg img-placehold
+router.get('/getimg/:size', getimg.index);
 
 module.exports = router;
