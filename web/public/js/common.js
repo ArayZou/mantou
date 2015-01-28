8d179b97a9831c21faa02980a1927cdd03f9dbba
rzou <rzou@Ctrip.com>
2015-01-28 21:21:17 +0800

    common js

--- web/public/js/common.js
+++ web/public/js/common.js
@@ -1,7 +1,11 @@
-$(function(){
-    $('body').on('click','#userlogout',function(){
-        $.post('/user/logout',function(){
-            window.location.href = '/';
+require(['js/conf'], function() {
+    require(['jquery'], function($) {
+        $(function(){
+            $('body').on('click','#userlogout',function(){
+                $.post('/user/logout',function(){
+                    window.location.href = '/';
+                });
+            });
         });
     });
 });
