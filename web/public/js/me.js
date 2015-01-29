b9884764b8e178bde5b8b0df3a5d2d8159bbca47
loveLibra <github_xq@126.com>
2015-01-29 22:04:40 +0800

    little change

--- web/public/js/me.js
+++ web/public/js/me.js
@@ -1,5 +1,11 @@
 require(['js/conf'], function() {
     require(['jquery', 'underscore', 'bootstrap'], function($, _) {
         console.log('loaded')
+        $(function() {
+            $('#nav-tabs').delegate('li', 'click', function() {
+                $('#nav-tabs li').removeClass('active');
+                $(this).addClass('active');
+            })
+        })
     })
 });
