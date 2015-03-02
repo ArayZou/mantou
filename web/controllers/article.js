42a1566c7362c3b8872dee894955d6a3b410e7a5
r_zou <r_zou@ctrip.com>
2015-03-02 11:37:11 +0800

    markdown 编辑器，编译显示完成

--- web/controllers/article.js
+++ web/controllers/article.js
@@ -5,6 +5,7 @@ require('../models/post');
 require('../models/group');
 Post = mongoose.model('Post');
 Group = mongoose.model('Group');
+var marked = require('marked');
 module.exports = function(req, res) {
     var groupName = req.params.groupname;
         articleid = req.params.articleid;
@@ -25,6 +26,9 @@ module.exports = function(req, res) {
                 postArray = post;
                 Post.findOne({postId: articleid}).populate({path:'floor.user group'}).exec(function(err, post) {
                     thisArticle = post;
+                    for(var i= 0;i<thisArticle.floor.length;i++){
+                        thisArticle.floor[i].content = marked(thisArticle.floor[i].content);
+                    }
                     res.render('article', {
                         js:[{js:'group'}],
                         title: 'article-'+groupName,
